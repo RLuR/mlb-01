cc9dbb4dad330a5c11e52f8044bc545ddeb3549b
raphael <raphael.lutz@gmx.at>
2021-09-09 19:13:15 +0200

    implemented monte carlo knn

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import models.knn
+import utils
+import numpy as np
 
 
 def test_classifier():
+    target_column = "class"
+
     # load dataset
-
+    df = utils.load_csv_data("data/iris.csv")
     # clean dataset
+    df = utils.clean_dataset(df)
 
-    # normalize dataset
+    labels = np.unique(df["class"])
+    df["class"].replace({labels[0]: 0, labels[1]: 1, labels[2]: 2}, inplace=True)
 
-    # split dataset
+    X, Y = utils.get_features_and_targets(df)
 
+    # normalize features
+    X = utils.normalize_features(X)
+
+    X_train, Y_train, X_test, Y_test = utils.monte_carlo_split(X, Y)
     # train and test model
+    Y_predict = models.knn.classify(X_train, Y_train, X_test, 5)
 
+    print(Y_predict)
+    print(Y_test)
     # evaluate
 
 
 def test_regressor():
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    test_classifier()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from numpy.random import default_rng
+
+rng = default_rng(1000)
+
+
+def load_csv_data(path: str, seperator=";") -> pd.DataFrame:
+    return pd.read_csv(path, sep=seperator)
+
+
+def clean_dataset(df: pd.DataFrame) -> pd.DataFrame:
+    return df.dropna()
+
+
+def get_features_and_targets(df: pd.DataFrame, target_name="class") -> (pd.DataFrame, pd.Series):
+    return df.drop(target_name, axis=1), df[target_name]
+
+
+def normalize_features(df: pd.DataFrame) -> pd.DataFrame:
+    return (df-df.min())/(df.max()-df.min())
+
+
+def monte_carlo_split(X: pd.DataFrame, Y: pd.DataFrame, test_ratio = 0.2) -> (pd.DataFrame, pd.Series, pd.DataFrame, pd.Series):
+    test_size =  round(len(X) * test_ratio)
+    random_indexes = rng.choice(len(X) - 1, size= test_size, replace=False)
+    return X.drop(random_indexes),  Y.drop(random_indexes), X.iloc[random_indexes], Y.iloc[random_indexes]
